c110ddf151ffb8d3f9b932f7267de0b75c0d2180
derekadrumm <dadrumm@wpi.edu>
2024-05-29 12:29:39 -0400

    fixed hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 def hello_world():
 	return "Hello World!"
 
+# return the string "Hello World! Hello World! ... Hello World!" N times
 def hello_world_n(N):
-	strN = "Hello World!"
+	str0 = "Hello World!"
+	strN = str0
 	for i in range(N-1):
-		strN = strN + ' ' + strN
-		
+		strN = strN + ' ' + str0
+	return strN
